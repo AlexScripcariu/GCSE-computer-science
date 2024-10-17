c30b95c1179de99901cec76620400a9df99f9454
Alex Scripcariu <alexscripcariu7@gmail.com>
2024-10-17 20:02:21 +0100

    added more to main() and started to modularise it all

--- projects/maths_34/main.py
+++ projects/maths_34/main.py
@@ -1,25 +1,45 @@
-import projects.maths_34.math_formulae as formulas
+import math_formulae
 
-OPTION_FORMULA = ["Surface Area", "Area", "Volume", "Trigonometry"]
+
+# basic option library contaning each possibility
+#stage 1
+OPTION_FORMULA = ["Surface Area", "Volume", "Area", "Trigonometry"]
+#stage 2
 OPTION_SHAPE_3D = ["cube", "rectangular prism", "cylinder",
                              "cone", "sphere", "hemisphere"]
 OPTION_SHAPE_2D = ["square", "rectangle", "trapezoid", "triangle",
                    "circle", "ellipse"]
 OPTION_TRIGONOMETRY = ["cosine rule", "triangle"]
-
+#accessiblity libraries (saves memory and speed with fast look ups)
+OPTIONS = [OPTION_SHAPE_3D, OPTION_SHAPE_2D, OPTION_TRIGONOMETRY]
+OPTIONS_CALLBACK = [[cube_surface_area, rectangular_prism_surface_area],
+                    [],
+                    [square_area, rectangle_area, trapezoid_area,
+                     triangle_area, circle_area, ellipse_area],
+                    [cosine_rule_side, triangle_area_trigonometric]]
 
 def display_items(options: list[str]) -> int:
     selected = -1
-    while selected != -1:
-        for i,v in enumerate(options):
+    for i,v in enumerate(options):
             print(f"{i + 1}) {v.title()}")
+    while not(1 <= selected <= len(options)):
         selected = int(input("Please select an option: "))
-        if 1 <= selected <= len(options):
-            return selected
+
+    return selected
 
 
 def main():
     print("Math-a-matics\n" + "-" * 20)
+    sub_section_choice = display_items(OPTION_FORMULA)
+    sub_section = 0
+    match sub_section_choice:
+        case 1 | 2:
+            sub_section = OPTIONS[0]
+        case 3:
+            sub_section = OPTIONS[2]
+        case 4:
+            sub_section = OPTIONS[3]
+    
 
 
 if __name__ == "__main__":
